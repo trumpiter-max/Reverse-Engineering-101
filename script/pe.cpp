487232d9055c2c4ad215781c4bf670882c5d782d
Hung Le Huy <huyhungvtu03@gmail.com>
2022-12-24 08:36:50 +0700

    fix error section name not display

--- script/pe.cpp
+++ script/pe.cpp
@@ -1,140 +1,107 @@
 #include <iostream>
-#include <fstream>
-#include <Windows.h>
-#include "Winnt.h"
-#include "iosfwd"
+#include <windows.h>
 
 using namespace std;
 
-void fileInformation(ifstream& file, IMAGE_NT_HEADERS ntHeaders)
-{
-    cout << "== File Information ==" << endl;
-    cout << "PointerToEntryPoint: 0x" << hex << ntHeaders.OptionalHeader.AddressOfEntryPoint << endl;
-    cout << "CheckSum: 0x" << hex << ntHeaders.OptionalHeader.CheckSum << endl;
-    cout << "ImageBase: 0x" << hex << ntHeaders.OptionalHeader.ImageBase << endl;
-    cout << "FileAlignment: 0x" << hex << ntHeaders.OptionalHeader.FileAlignment << endl;
-    cout << "SizeOfImage: 0x" << hex << ntHeaders.OptionalHeader.SizeOfImage << endl;
-    cout << "==================================================" << endl << endl;
-}
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    cerr << "Usage: " << argv[0] << " <path-of-PE-file>" << endl;
+    return 1;
+  }
 
-void sectionInformation(ifstream& file, IMAGE_NT_HEADERS ntHeaders)
-{
-    IMAGE_SECTION_HEADER sectionHeader;
+  // Open the PE file
+  HANDLE hFile = CreateFileA(argv[1], GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (hFile == INVALID_HANDLE_VALUE) {
+    cerr << "Error: Unable to open file " << argv[1] << endl;
+    return 1;
+  }
 
-    cout << "== Section Information ==" << endl;
-    for (int i = 0; i < ntHeaders.FileHeader.NumberOfSections; i++)
-    {
-        file.read(reinterpret_cast<char*>(&sectionHeader), sizeof(sectionHeader));
-        cout << "Name: " << sectionHeader.Name << endl;
-        cout << "Characteristics: 0x" << hex << sectionHeader.Characteristics << endl;
-        cout << "RawAddress: 0x" << hex << sectionHeader.PointerToRawData << endl;
-        cout << "RawSize: 0x" << hex << sectionHeader.SizeOfRawData << endl;
-        cout << "VirtualAddress: 0x" << hex << sectionHeader.VirtualAddress << endl;
-        cout << "VirtualSize: 0x" << hex << sectionHeader.Misc.VirtualSize << endl;
-        cout << "--------------------------------------------------" << endl << endl;
+  // Map the PE file into memory
+  HANDLE hMapping = CreateFileMappingA(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
+  if (hMapping == NULL) {
+    cerr << "Error: Unable to create file mapping for " << argv[1] << endl;
+    CloseHandle(hFile);
+    return 1;
+  }
+
+  LPVOID lpBase = MapViewOfFile(hMapping, FILE_MAP_READ, 0, 0, 0);
+  if (lpBase == NULL) {
+    cerr << "Error: Unable to map view of file for " << argv[1] << endl;
+    CloseHandle(hMapping);
+    CloseHandle(hFile);
+    return 1;
+  }
+
+  // Get the DOS header
+  PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)lpBase;
+  if (pDosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
+    cerr << "Error: Invalid DOS signature in " << argv[1] << endl;
+    UnmapViewOfFile(lpBase);
+    CloseHandle(hMapping);
+    CloseHandle(hFile);
+    return 1;
+  }
+
+  // Get the NT headers
+  PIMAGE_NT_HEADERS pNtHeaders = (PIMAGE_NT_HEADERS)((BYTE*)lpBase + pDosHeader->e_lfanew);
+  if (pNtHeaders->Signature != IMAGE_NT_SIGNATURE) {
+    cerr << "Error: Invalid NT signature in " << argv[1] << endl;
+    UnmapViewOfFile(lpBase);
+    CloseHandle(hMapping);
+    CloseHandle(hFile);
+    return 1;
+  }
+
+  cout << endl << "== Information of file ==" << endl;
+  // Get the NT headers
+  DWORD PointerToEntryPoint = pNtHeaders->OptionalHeader.AddressOfEntryPoint;
+  cout << "PointerToEntryPoint: " << dec << PointerToEntryPoint << endl;
+  DWORD CheckSum = pNtHeaders->OptionalHeader.CheckSum;
+  cout << "CheckSum: " << dec << CheckSum << endl;
+  DWORD ImageBase = pNtHeaders->OptionalHeader.ImageBase;
+  cout << "ImageBase: " << dec << ImageBase << endl;
+  DWORD FileAlignment = pNtHeaders->OptionalHeader.FileAlignment;
+  cout << "FileAlignment: " << dec << FileAlignment << endl;
+  DWORD SizeOfImage = pNtHeaders->OptionalHeader.SizeOfImage;
+  cout << "SizeOfImage: " << dec << SizeOfImage << endl;
+  cout << "----------------------------------------" << endl << endl;
+
+  cout << "== Information of sections ==" << endl;
+  // Get the section headers
+  PIMAGE_SECTION_HEADER pSectionHeader = IMAGE_FIRST_SECTION(pNtHeaders);
+  for (unsigned int i = 0; i < pNtHeaders->FileHeader.NumberOfSections; i++, pSectionHeader++) {
+    cout << "Name: " << pSectionHeader->Name << endl;
+    cout << " Characteristics: " << dec << pSectionHeader->Characteristics << endl;
+    cout << " RawAddress: " << dec << pSectionHeader->PointerToRawData << endl;
+    cout << " RawSize: " << dec << pSectionHeader->SizeOfRawData << endl;
+    cout << " VirtualAddress: " << dec << pSectionHeader->VirtualAddress << endl;
+    cout << " VirtualSize: " << dec << pSectionHeader->Misc.VirtualSize << endl;
+    cout << "----------------------------------------" << endl << endl;
+  }
+
+  cout << endl << "== Information of imports ==" << endl;
+  // Get the import table (not woking)
+  PIMAGE_DATA_DIRECTORY pImportTable = &pNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+  PIMAGE_IMPORT_DESCRIPTOR pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR)((BYTE*)lpBase + pImportTable->VirtualAddress);
+  for (; pImportDesc->Name != 0; pImportDesc++) {
+    cout << "DLL name: " << (char*)((BYTE*)lpBase + pImportDesc->Name) << endl;
+    // Get the import address table
+    PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)((BYTE*)lpBase + pImportDesc->FirstThunk);
+    // Get the import name table
+    PIMAGE_THUNK_DATA pName = (PIMAGE_THUNK_DATA)((BYTE*)lpBase + pImportDesc->OriginalFirstThunk);
+    for (; pThunk->u1.Function != 0; pThunk++, pName++) {
+      if (IMAGE_SNAP_BY_ORDINAL(pName->u1.Ordinal)) {
+        cout << " Ordinal: " << IMAGE_ORDINAL(pName->u1.Ordinal) << endl;
+      } else {
+        PIMAGE_IMPORT_BY_NAME pImportName = (PIMAGE_IMPORT_BY_NAME)((BYTE*)lpBase + pName->u1.AddressOfData);
+        cout << " Function name: " << pImportName->Name << endl;
+      }
     }
-    cout << "==================================================" << endl << endl;
-}
+  }
+  // Unmap the PE file
+  UnmapViewOfFile(lpBase);
+  CloseHandle(hMapping);
+  CloseHandle(hFile);
 
-void importInformation(ifstream& file, IMAGE_NT_HEADERS ntHeaders)
-{
-    IMAGE_DATA_DIRECTORY importDirectory = ntHeaders.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
-    IMAGE_IMPORT_DESCRIPTOR importDescriptor;
-
-    if (importDescriptor.Name == 0)
-    {
-        cout << "The file does not have any imports." << endl;
-        return;
-    }
-
-    cout << "== Import Information ==" << endl;
-    file.seekg(importDirectory.VirtualAddress, ios::beg);
-    file.read(reinterpret_cast<char*>(&importDescriptor), sizeof(importDescriptor));
-
-    while (importDescriptor.Name != 0)
-    {
-        cout << "Name: " << importDescriptor.Name << endl;
-        cout << "OriginalFirstThunk: 0x" << hex << importDescriptor.OriginalFirstThunk << endl;
-        cout << "TimeDateStamp: 0x" << hex << importDescriptor.TimeDateStamp << endl;
-        cout << "ForwarderChain: 0x" << hex << importDescriptor.ForwarderChain << endl;
-        cout << "FirstThunk: 0x" << hex << importDescriptor.FirstThunk << endl;
-        cout << "--------------------------------------------------" << endl << endl;
-
-        file.read(reinterpret_cast<char*>(&importDescriptor), sizeof(importDescriptor));
-    }
-    cout << "==================================================" << endl << endl;
-}
-
-void exportInformation(ifstream& file, IMAGE_NT_HEADERS ntHeaders)
-{
-
-    IMAGE_DATA_DIRECTORY exportDirectory = ntHeaders.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
-    IMAGE_EXPORT_DIRECTORY exportDescriptor;
-
-    // Check if the export directory exists
-    if (exportDirectory.VirtualAddress == 0 || exportDirectory.Size == 0)
-    {
-        cout << "The file does not have an export directory." << endl;
-        return;
-    }
-
-    // Read the export descriptor
-    file.seekg(exportDirectory.VirtualAddress, ios::beg);
-    file.read(reinterpret_cast<char*>(&exportDescriptor), sizeof(exportDescriptor));
-
-    cout << "== Export Information ==" << endl;
-    cout << "Name: " << exportDescriptor.Name << endl;
-    cout << "Base: " << exportDescriptor.Base << endl;
-    cout << "NumberOfFunctions: " << exportDescriptor.NumberOfFunctions << endl;
-    cout << "NumberOfNames: " << exportDescriptor.NumberOfNames << endl;
-    cout << "AddressOfFunctions: 0x" << hex << exportDescriptor.AddressOfFunctions << endl;
-    cout << "==================================================" << endl << endl;
-}
-
-int main(int argc, char* argv[])
-{
-    if (argc < 2)
-    {
-        cout << "Usage: .\\PE <path-of-PE-file>" << endl;
-        return 1;
-    }
-
-    // Open the file in binary mode
-    ifstream file(argv[1], ios::binary);
-    if (!file.is_open())
-    {
-        cout << "Failed to open file, try again" << endl;
-        return 1;
-    }
-
-    // Read in the DOS header
-    IMAGE_DOS_HEADER dosHeader;
-    file.read(reinterpret_cast<char*>(&dosHeader), sizeof(dosHeader));
-
-    // Check the magic number to verify that this is a PE file
-    if (dosHeader.e_magic != IMAGE_DOS_SIGNATURE)
-    {
-        cout << "Not a valid PE file" << endl;
-        return 1;
-    }
-
-    // Read in the NT headers
-    file.seekg(dosHeader.e_lfanew, ios::beg);
-    IMAGE_NT_HEADERS ntHeaders;
-    file.read(reinterpret_cast<char*>(&ntHeaders), sizeof(ntHeaders));
-
-    // Check the magic number to verify that this is a PE file
-    if (ntHeaders.Signature != IMAGE_NT_SIGNATURE)
-    {
-        cout << "Not a valid PE file" << endl;
-        return 1;
-    }
-
-    // Print out information
-    fileInformation(file, ntHeaders);
-    sectionInformation(file, ntHeaders);
-    importInformation(file, ntHeaders);
-    exportInformation(file, ntHeaders);
-
-    return 0;
+  return 0;
 }
