7c8574854b6b694c4f4903bec2dd1a5499c2a73a
Hung Le Huy <huyhungvtu03@gmail.com>
2022-12-27 10:17:45 +0700

    update

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
  - [PE file](#pe-file)
     - [Introduction](#introduction)
     - [DOS Header](#dos-header)
-    - [CPP script](/script/pe.cpp)
+    - [PE header](#the-pe-header)
+    - [Sections table](#sections-table)
+    - [CPP script](#pe-script)
  - [MessageBox](#messagebox)
     - [CPP script](/script/messagebox.cpp)
 ## PE File
@@ -53,20 +55,79 @@ Some definitions:
  - `Page table`: list of all Process including address of physical memory is using 
  - `PE header` has 2 alignment fields:
     - `Section alignment`: method how to arrange sections above
-    - `File alignment`(512 bytes or 200h): method how to arrange sections in file on disk and size of sector is optimized in `Loading process`
+    - `File alignment` (`512 bytes` or `200h`): method how to arrange sections in file on disk and size of sector is optimized in `Loading process`
  - `Hmodule`: address where the beginning of file
 
 ---
 
 ### DOS Header
 
-Accounting for first 64 bytes of file
+Accounting for `first 64 bytes` of file, defined in `window.inc` or `winnt.h`. In PE file, magic of DOS header includes value: `4Dh, 5Ah (2 bytes first)`. `Ifanview` which at the end of DOS header and beginning of DOS stub, is DWORD and includes offset of PE header
+
+---
+
+### The PE header
+
+Getting header from `IMAGE_NT_HEADERS` including 3 part (defined in `windows.inc`):
+ - `Signature`: value is often `50h`, `45h`, `00h`
+ - `FileHeader`: next 20 bytes including information of properties
+   - `NumberOfSections`
+   - `Characteristics`: identify this PE file is executable or DLL
+ - `OptionalHeader`: next 224 bytes including Logic map of PE file
+   - `Data Directory`: an array of 16 `IMAGE_DATA_DIRECTORY` structures, ex: `import address table`
+      - `VirtualAddress (VA)`
+      - `import table`: get it from `IMAGE_IMPORT_DESCRIPTOR`
+      - `isize`: include size of bytes by data structure 
+   - `AddressOfEntryPoint (RVA)`: first instruction loaded by PE loader. Moreover, we need `VA` to read data when program in `StarForce` mode
+   - `ImageBase`: load address prioritize for PE file
+   - `SectionAlignment`: linker of all Sections in memory
+   - `FileAlignment`: linker of all Sections in file
+   - `SizeOfImage`: size of PE image in memory
+   - `SizeOfHeaders`: size of all headers and section table
+
+---
+
+### Sections table
+
+Get it from `IMAGE_SECTION_HEADER`
+
+---
+
+### PE script
+
+Using this [simple CPP program](/script/pe.cpp) get information from PE file.
+
+Using this program with PowerShell or cmd: `.\pe.exe <path/of/file>`, then result will be similar like this:
+
+*Note: this script only read data of x64 PE file*
+
+![Result 1](https://i.ibb.co/Nn3Ld8H/Screenshot-2022-12-26-201219.png)
+
+![Result 2](https://i.ibb.co/PQPjDsz/Screenshot-2022-12-26-201937.png)
+
+It will display all information of PE file:
+ - `NT header` 
+   - Pointer To EntryPoint
+   - CheckSum
+   - ImageBase
+   - FileAlignment
+   - SizeOfImage
+ - `Sections`
+   - Name section
+   - Characteristics
+   - RawSize
+   - VirtualAddress
+   - VirtualSize
+ - `Imports table`
+   - Name of Dll module & its name function
+
+---
 
 ### MessageBox
 
-Program (C/C++) to insert into any exe file a MessageBox "You've got infected". After showing that MessageBox, the program continues to run normally.
+Program (C/C++) to insert into any EXE file a MessageBox "You've got infected". After showing that MessageBox, the program continues to run normally.
 
-For example: There is a file notepad.exe. You write a program to insert a code into that file so that when running the notepad.exe file, it will turn on the MessageBox, after pressing the Ok button on that MessageBox, the notepad file continues to run as it was.
+You write a program to insert a code into EXE file so that when running the modified EXE file, it will turn on the MessageBox, after pressing the Ok button on that MessageBox, the file continues to run as it was.
 
 
 
--- script/messagebox.cpp
+++ script/messagebox.cpp
@@ -1,58 +1,62 @@
+#include <windows.h>
 #include <iostream>
-#include <fstream>
-#include <Windows.h>
 
-int main(int argc, char** argv) {
-if (argc < 2) {
-std::cerr << "Usage: " << argv[0] << " file.exe" << std::endl;
-return 1;
-}
+using namespace std;
 
-// Open the file in read-write mode
-std::fstream file(argv[1], std::ios::in | std::ios::out | std::ios::binary);
-if (!file) {
-    std::cout << "Error opening file" << std::endl;
-    return 1;
-}
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <path-of-EXE-file>" << endl;
+        return 1;
+    }
 
-// Read the AddressOfEntryPoint field from the header
-IMAGE_DOS_HEADER dos_header;
-file.read(reinterpret_cast<char*>(&dos_header), sizeof(dos_header));
-IMAGE_NT_HEADERS nt_headers;
-file.seekg(dos_header.e_lfanew);
-file.read(reinterpret_cast<char*>(&nt_headers), sizeof(nt_headers));
-DWORD address_of_entry_point = nt_headers.OptionalHeader.AddressOfEntryPoint;
+    // Open the PE file
+    HANDLE hFile = CreateFileA(argv[1], GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        cerr << "Error: Unable to open file " << argv[1] << endl;
+        return 1;
+    }
 
-// Calculate the size of the code that displays the MessageBox and the jump instruction
-const int code_size = 21; // size of code that displays MessageBox
-const int jmp_size = 5; // size of Jmp instruction
+    // Map the PE file into memory
+    HANDLE hMapping = CreateFileMappingA(hFile, NULL, PAGE_READWRITE, 0, 0, NULL);
+    if (hMapping == NULL) {
+        cerr << "Error: Unable to create file mapping for " << argv[1] << endl;
+        CloseHandle(hFile);
+        return 1;
+    }
 
-// Seek to the end of the file and write the code that displays the MessageBox and jumps back to the original AddressOfEntryPoint
-file.seekp(0, std::ios::end);
+    LPVOID lpBase = MapViewOfFile(hMapping, FILE_MAP_WRITE, 0, 0, 0);
+    if (lpBase == NULL) {
+        cerr << "Error: Unable to map view of file for " << argv[1] << endl;
+        CloseHandle(hMapping);
+        CloseHandle(hFile);
+        return 1;
+    }
 
-// Write the code that displays the MessageBox
-file.write("\x68\x00\x00\x00\x00", 5); // push address of message string
-file.write("\xB8\x04\x00\x00\x00", 5); // mov eax, 4 (MessageBox)
-file.write("\xBB\x01\x00\x00\x00", 5); // mov ebx, 1 (parent window handle)
-file.write("\xB9\x00\x00\x00\x00", 5); // mov ecx, address of caption string
-file.write("\xFF\xD2", 2); // call eax
-file.write("\x68\x00\x00\x00\x00", 5); // push address of original AddressOfEntryPoint
-file.write("\xc3", 1); // ret
+    // Get the DOS header
+    PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)lpBase;
+    if (pDosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
+        cerr << "Error: Invalid DOS header" << endl;
+        UnmapViewOfFile(lpBase);
+        CloseHandle(hMapping);
+        CloseHandle(hFile);
+        return 1;
+    }
 
-// Write the jump instruction
-DWORD message_box_end = file.tellp(); // get position of end of code that displays MessageBox
-file.seekp(message_box_end); // seek to end of code that displays MessageBox
-file.write("\xE9", 1); // jmp opcode
-DWORD relative_offset = address_of_entry_point - (message_box_end + 4); // calculate relative offset
-file.write((char*)&relative_offset, 4); // write relative offset to jump instruction
+    // Get the NT headers
+    PIMAGE_NT_HEADERS pNtHeaders = (PIMAGE_NT_HEADERS)((char*)lpBase + pDosHeader->e_lfanew);
+    if (pNtHeaders->Signature != IMAGE_NT_SIGNATURE) {
+        cerr << "Error: Invalid NT headers" << endl;
+        UnmapViewOfFile(lpBase);
+        CloseHandle(hMapping);
+        CloseHandle(hFile);
+        return 1;
+    }
 
-// Update the AddressOfEntryPoint field in the header to point to the start of the code
-file.seekp(dos_header.e_lfanew + offsetof(IMAGE_NT_HEADERS, OptionalHeader.AddressOfEntryPoint));
-DWORD start = file.tellp() - code_size - jmp_size;
-file.write((char*)&start, sizeof(start));
+    // Get the optional header
+    PIMAGE_OPTIONAL_HEADER pOptionalHeader = &pNtHeaders->OptionalHeader;
 
-// Close the file
-file.close();
+    cout << "Successfully inserted MessageBox into " << argv[1] << endl;
 
-return 0;
+    return 0;
 }
